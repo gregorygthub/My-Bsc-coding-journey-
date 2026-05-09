6ca12c20bcdc2e2f30b0d889c81af98277a058ae
gregorygthub <gwagongrggnn@gmail.com>
2026-05-09 16:01:57 +0300

    Day 5: Fix division by zero bug

--- calculator.py
+++ calculator.py
@@ -3,4 +3,8 @@ num2 = int(input("Enter second number: "))
 print(num1, "+", num2, "=", num1 + num2)
 print(num1, "-", num2, "=", num1 - num2)
 print(num1, "*", num2, "=", num1 * num2)
-print(num1, "/", num2, "=", num1 / num2)
+if num2 != 0:
+    print(num1, "/", num2, "=", num1 / num2)
+else:
+    print("Error: Cannot divide by zero")
+
